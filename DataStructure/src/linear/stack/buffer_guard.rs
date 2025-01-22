8329a5c0a4ad415393286b250cfb726eef810033
Alessandro Mesti <mesti.alessandro.git@gmail.com>
2025-01-22 13:15:26 +0100

    Added a buffer guard to safely handle a failure during array re-allocation in Stack struct avoiding memory leaks caused by not de-allocating unused new allocated array

---
+++ DataStructure/src/linear/stack/buffer_guard.rs
@@ -0,0 +1,35 @@
+use std::alloc::{alloc, dealloc, Layout};
+use std::mem;
+use std::ptr::NonNull;
+
+pub struct BufferGuard<T> {
+    buffer: NonNull<T>,
+    layout: Layout,
+}
+
+impl<T> BufferGuard<T> {
+    pub fn new(size: usize) -> BufferGuard<T> {
+        let layout = Layout::array::<T>(size).unwrap();
+        let buffer = NonNull::new(unsafe { alloc(layout) } as _).expect("Failed to allocate buffer");
+        BufferGuard { buffer, layout }
+    }
+
+    pub fn as_ptr(&self) -> *mut T {
+        self.buffer.as_ptr()
+    }
+
+    pub fn into_inner(mut self) -> (NonNull<T>, Layout) {
+        let buffer = self.buffer;
+        let layout = self.layout;
+        mem::forget(self);
+        (buffer, layout)
+    }
+}
+
+impl<T> Drop for BufferGuard<T> {
+    fn drop(&mut self) {
+        unsafe {
+            dealloc(self.buffer.as_ptr() as _, self.layout);
+        }
+    }
+}

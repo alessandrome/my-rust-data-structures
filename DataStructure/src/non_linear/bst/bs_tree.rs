f8d9cb6a119f59c562d7f2aa43b010feb6d13092
Alessandro Mesti <mesti.alessandro.git@gmail.com>
2025-02-02 14:28:42 +0100

    Some BSTree Tests - Corrected find function

--- DataStructure/src/non_linear/bst/bs_tree.rs
+++ DataStructure/src/non_linear/bst/bs_tree.rs
@@ -3,11 +3,16 @@ use super::node::BSNode;
 
 pub struct BSTree<T: PartialOrd + PartialEq> {
     root: Option<Box<BSNode<T>>>,
+    size: usize,
 }
 
 impl<T: PartialOrd + PartialEq> BSTree<T> {
     pub fn new() -> Self {
-        Self { root: None }
+        Self { root: None, size: 0 }
+    }
+
+    pub fn size(&self) -> usize {
+        self.size
     }
 
     pub fn insert(&mut self, value: T) {
@@ -19,12 +24,17 @@ impl<T: PartialOrd + PartialEq> BSTree<T> {
             // Tree must be traversed and then inserted the node
             let mut checking_boxed_node = self.root.as_mut().unwrap();
             while true {
+                if new_boxed_node.value == checking_boxed_node.value {
+                    //Value already exists in the tree - No insert
+                    return;
+                }
                 if new_boxed_node.value < checking_boxed_node.value {
                     // Must insert or traverse left subtree
                     let left_node_opt = checking_boxed_node.left_mut();
                     match left_node_opt {
                         None => {
                            *left_node_opt = Some(new_boxed_node);
+                            self.size += 1;
                             break;
                         }
                         Some(left_node) => {
@@ -32,12 +42,12 @@ impl<T: PartialOrd + PartialEq> BSTree<T> {
                         }
                     }
                 } else {
-
                     // Must insert or traverse right subtree
                     let right_node_opt = checking_boxed_node.right_mut();
                     match right_node_opt {
                         None => {
                             *right_node_opt = Some(new_boxed_node);
+                            self.size += 1;
                             break;
                         }
                         Some(right_node) => {
@@ -50,9 +60,15 @@ impl<T: PartialOrd + PartialEq> BSTree<T> {
     }
 
     pub fn find(&self, value: T) -> Option<&T> {
-        while let Some(node) = &self.root {
+        let mut checking_boxed_node = self.root.as_ref();
+        while let Some(node) = checking_boxed_node {
             if node.value == value {
                 return Some(&node.value);
+            }
+            if value < node.value {
+                checking_boxed_node = node.left().as_ref();
+            } else {
+                checking_boxed_node = node.right().as_ref();
             }
         }
         None
---
+++ DataStructure/src/non_linear/bst/tests.rs
@@ -0,0 +1,37 @@
+use crate::non_linear::bst::bs_tree::BSTree;
+
+const TREE_VALUES: [i32; 9] = [5, -1, -2, -3, 7, -9, 0, 10, 4];
+const TREE_DISPLAY: &'static str = "[-9, -3, -2, -1, 0, 4, 5, 7, 10]";
+
+fn create_empty_tree() -> BSTree<i32> {
+    BSTree::<i32>::new()
+}
+
+fn create_tree() -> BSTree<i32> {
+    let mut tree = BSTree::<i32>::new();
+    for val in TREE_VALUES {
+        tree.insert(val);
+    }
+    tree
+}
+
+#[test]
+fn test_find() {
+    let tree = create_tree();
+    let result = tree.find(7);
+    assert!(result.is_some());
+    assert_eq!(*result.unwrap(), 7);
+    let result = tree.find(-545);
+    assert!(result.is_none());
+}
+
+#[test]
+fn test_insert() {
+    let mut tree = create_tree();
+    let start_size = tree.size();
+    let result = tree.insert(99);
+    assert_eq!(tree.size(), start_size + 1);
+    let result = tree.find(99);
+    assert_eq!(tree.size(), start_size + 1);
+}
+
